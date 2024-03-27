00af296d9a3bb21345b0c7f7b9a3f57697714392
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-03-27 03:04:43 +0300

    Added contexts

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random() * 20)+1;
 let score = 20;
-let highscore = 20;
+let highscore = 0;
 
 const displayMessage = function (message){
     document.querySelector('.message').textContent = message;
@@ -10,4 +10,17 @@ const displayMessage = function (message){
 
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    console.log(secretNumber, typeof secretNumber);
+
+    if (secretNumber === guess) {
+        displayMessage('Doğru tahmin ettin!');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    };
+
 });
+
