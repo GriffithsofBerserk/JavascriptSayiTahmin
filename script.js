908f93356fd61df922ef3fe2b9f7cdda1dc87173
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-03-31 18:17:12 +0300

    added rights section

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 100)+1;
-let score = 20;
+let secretNumber = Math.trunc(Math.random() * 10)+1;
+let score = 0;
 let highscore = 0;
+let rights = 3;
 
 const displayMessage = function (message){
     document.querySelector('.message').textContent = message;
@@ -20,13 +21,28 @@ document.querySelector('.check').addEventListener('click',function(){
 
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
-    };
 
-    if (score>highscore) {
+        score +=1;
+        document.querySelector('.label-scoreNumber').textContent = score;
         highscore = score;
         document.querySelector('.label-highscoreNumber').textContent = highscore;
     }
-    
+     else {
+        rights -= 1;
+        document.querySelector('.rights-number').textContent = rights;
+        displayMessage('Tekrar Deneyin!');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#ff0000';
+        document.querySelector('.number').style.width = '30rem';
+        score = 0;
+        document.querySelector('.label-scoreNumber').textContent = score;
+
+        if (rights === 0) {
+            rights = 0;
+            document.querySelector('.rights-number').textContent = rights;
+        }
+    };
 
 });
 
